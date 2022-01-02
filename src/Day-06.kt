276b23867fd1f56a6ac9f4c6c81d0dc5d1349302
Tomas Eriksson <tomas.eriksson@optimation.se>
2022-01-02 12:35:02 +0100

    Day 6 improved output

--- src/Day-06.kt
+++ src/Day-06.kt
@@ -1,16 +1,13 @@
 import java.io.File
 
-
-
 fun main()  {
     val fishes=File("data/Day-06-data.txt").readText().split(',').map{ it.toInt() }
     val fishState = Array<Long>(9) { 0 }
-
     for(fish in fishes) {
         fishState[fish]=fishState.getOrElse(fish) { 0 }+1
     }
-    println(fishState)
 
+    println("Day 6:")
     for(day in 1.rangeTo(256)) {
         val fishState0=fishState[0]
         for(i in 1.rangeTo(fishState.lastIndex)) {
@@ -18,6 +15,7 @@ fun main()  {
         }
         fishState[6]+=fishState0
         fishState[8]=fishState0
-        println("$day: ${fishState.sum().toString().padStart(12,' ')} [${fishState.map{ it.toString().padStart(4,' ')}.joinToString()}]")
+        if( day in listOf(18,80,256))
+            println("${day.toString().padStart(3,' ')}: ${fishState.sum().toString().padStart(12,' ')} [${fishState.map{ it.toString().padStart(4,' ')}.joinToString()}]")
     }
 }
