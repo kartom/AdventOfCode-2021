d4215647b5cb1d5cc78b5146dfafd737a0fb8f90
Tomas Eriksson <tomas.eriksson@optimation.se>
2022-01-07 20:45:09 +0100

    Day 3 improved

--- src/Day-03.kt
+++ src/Day-03.kt
@@ -1,51 +1,29 @@
-import java.io.File
-import kotlin.math.pow
-
-fun day3Part1(data: List<String>) {
-    val nX = MutableList(data[0].length) {0}
-    val nHalf:Int = data.size/2
-    for (str in data) {
-        for(i in 0 until nX.size) if( str[i]=='1' )  nX[i]+=1
-    }
-    var gamma = 0
-    for(n in nX) {
-        gamma = gamma shl 1
-        if (n>nHalf) gamma+=1
-    }
-    val epsilon = gamma xor ((2.0).pow(nX.size)-1).toInt()
-    println("Day 3, part 1: Gamma rate=$gamma, Epsilon rate=$epsilon, Power consumption=${gamma*epsilon}")
-}
-
-fun day3Part2(data: List<String>) {
-    //O2
-    var pos = 0
-    val o2data = mutableListOf<String>()
-    o2data.addAll(data)
-    while (o2data.size>1) {
-        var n = 0
-        for (str in o2data) if( str[pos]=='1')  n+=1
-        val digit = if (n>=(o2data.size+1)/2) '1' else '0'
-        o2data.removeIf { it[pos]!=digit }
-        pos+=1
-    }
-    val o2: Int = Integer.parseInt(o2data[0],2)
-    //CO2
-    pos=0
-    val co2data = mutableListOf<String>()
-    co2data.addAll(data)
-    while (co2data.size>1) {
-        var n = 0
-        for (str in co2data) if( str[pos]=='1')  n+=1
-        val digit = if (n<(co2data.size+1)/2) '1' else '0'
-        co2data.removeIf { it[pos]!=digit }
-        pos+=1
-    }
-    val co2: Int = Integer.parseInt(co2data[0],2)
-    println("Day 3, part 2: O2 generator=$o2, CO2 scrubber=$co2, Life support=${o2*co2}")
-}
+val pow2= listOf(1, 2, 4, 8, 16, 32, 64 , 128, 256, 512, 1024)
 
 fun main()  {
-    val data  = File("data/Day-03-data.txt").readLines()
-    day3Part1(data)
-    day3Part2(data)
+    val data  = java.io.File("data/Day-03-data.txt").readLines()
+    val bits = data[0].length
+
+    //Part 1
+    val nHalf:Int = data.size/2
+    val gamma = (0 until bits).sumOf { i -> if (data.count { it[i]=='1' }>nHalf) pow2[bits-i-1] else 0 }
+    val epsilon = gamma xor (pow2[bits]-1)
+    println("Day 3, part 1: Gamma rate=$gamma, Epsilon rate=$epsilon, Power consumption=${gamma*epsilon}")
+
+    //Part 2
+    val o2data = data.toMutableList()
+    for( pos in 0 until bits ) {
+        val n = o2data.count { it[pos]=='1' }
+        o2data.removeIf { it[pos]!=if (n>=(o2data.size+1)/2) '1' else '0' }
+        if( o2data.size == 1) break
+    }
+    val o2: Int = Integer.parseInt(o2data.first(),2)
+    val co2data = data.toMutableList()
+    for( pos in 0 until bits ) {
+        val n = co2data.count { it[pos]=='1' }
+        co2data.removeIf { it[pos]!=if (n<(co2data.size+1)/2) '1' else '0' }
+        if( co2data.size == 1) break
+    }
+    val co2: Int = Integer.parseInt(co2data.first(),2)
+    println("Day 3, part 2: O2 generator=$o2, CO2 scrubber=$co2, Life support=${o2*co2}")
 }
