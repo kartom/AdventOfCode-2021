8c355d88dc99ec2f9ee5bdfaac4d214890feb3c5
Tomas Eriksson <tomas.eriksson@optimation.se>
2022-01-08 00:02:48 +0100

    Day 7 improved

--- src/Day-07.kt
+++ src/Day-07.kt
@@ -1,18 +1,8 @@
-import java.io.File
-import kotlin.math.abs
-
-fun calcFuel(crabs: List<Int>, fuelFun: (Int)->Int):Int {
-    val fuel = Array(crabs.maxOf { it } +1) { 0 }
-    for(pos in fuel.indices) {
-        crabs.forEach { crab-> fuel[pos] += fuelFun(abs(crab - pos)) }
-    }
-
-    return fuel.minOf { it }
-}
+fun calcFuel(crabs: List<Int>, fuelFun: (Int)->Int):Int = (0..crabs.maxOf {it} ).map { pos ->
+        crabs.sumOf {  crab-> fuelFun( kotlin.math.abs(crab - pos)) }}.minOf { it }
 
 fun main()  {
-    val crabs= File("data/Day-07-data.txt").readText().split(',').map{ it.toInt() }
-    println("Day 7, part 2:${calcFuel(crabs,fun(n:Int)=n)}")
+    val crabs= java.io.File("data/Day-07-data.txt").readText().split(',').map{ it.toInt() }
+    println("Day 7, part 1:${calcFuel(crabs,fun(n:Int)=n)}")
     println("Day 7, part 2:${calcFuel(crabs,fun(n:Int)=(n*(1+n)/2))}")
 }
-
