3fe5222f4d6a6131d4e05300edc3fbd96e9241af
Tomas Eriksson <tomas.eriksson@optimation.se>
2022-01-07 23:36:42 +0100

    Day 5 improved

--- src/Day-05.kt
+++ src/Day-05.kt
@@ -1,47 +1,29 @@
-import java.io.File
-
-class Co(val x: Int, val y: Int) {
-    override fun toString(): String = "($x,$y)"
-}
+import kotlin.math.*
 
 class Line(str: String) {
-    private val coList = str.replace(" -> ",",").split(',').map { it.toInt() }
-    private val c = listOf(Co(coList[0],coList[1]),Co(coList[2],coList[3]))
+    private val c = str.replace(" -> ",",").split(',').map { it.toInt() }
 
-    fun isNotDiagonal(): Boolean = (c[0].x==c[1].x) or (c[0].y==c[1].y)
+    fun isNotDiagonal(): Boolean = (c[0]==c[2]) or (c[1]==c[3])
 
-    fun getLineList(): MutableList<Co> {
-        val res = mutableListOf<Co>()
-        val dx = if(c[1].x>c[0].x) 1 else (if(c[0].x>c[1].x) -1 else 0)
-        val dy = if(c[1].y>c[0].y) 1 else (if(c[0].y>c[1].y) -1 else 0)
-        var x  = c[0].x
-        var y  = c[0].y
-        res.add(Co(x,y))
-        while( (x!=c[1].x) or (y!=c[1].y)) {
-            x+=dx
-            y+=dy
-            res.add(Co(x,y))
-        }
-        return res
+    fun getLineList(): List<Pair<Int,Int>> {
+        val n = max(abs(c[2] - c[0]), abs(c[1]- c[3]))
+        return (0 ..n).map { i-> Pair(c[0]+(c[2] - c[0])*i/n,c[1]+(c[3] - c[1])*i/n) }
     }
 }
 
-fun countOverlap(lines: MutableList<Line>, diagonals: Boolean = false):Int {
+fun countOverlap(lines: List<Line>, diagonals: Boolean = false):Int {
     val diagram = mutableMapOf<String,Int>()
-    for( line in lines) {
-        if (diagonals or line.isNotDiagonal())
-            for (co in line.getLineList()) {
-                diagram[co.toString()] = diagram.getOrDefault(co.toString(), 0)+1
-            }
+    lines.forEach { line ->
+        if (diagonals or line.isNotDiagonal()) {
+            line.getLineList().forEach { (x, y) ->
+                diagram["$x,$y"] = diagram.getOrDefault("$x,$y", 0) + 1 }
+        }
     }
-    var count = 0
-    diagram.forEach { (_, value) -> if( value > 1 ) count+=1 }
-    return count
+    return diagram.count { it.value > 1  }
 }
 
 fun main()  {
-    val lines = mutableListOf<Line>()
-    File("data/Day-05-data.txt").readLines().forEach { lines.add(Line(it))  }
+    val lines = java.io.File("data/Day-05-data.txt").readLines().map { Line(it)  }
     println("Day5 part 1: ${countOverlap(lines, false)}")
     println("Day5 part 2: ${countOverlap(lines, true)}")
 }
