6435fe0b08eaf7ec2919ba787bc3bc61c9d99fa9
Tomas Eriksson <tomas.eriksson@optimation.se>
2022-01-07 23:52:51 +0100

    Day 6 improved

--- src/Day-06.kt
+++ src/Day-06.kt
@@ -1,18 +1,13 @@
-import java.io.File
 
 fun main()  {
-    val fishes= File("data/Day-06-data.txt").readText().split(',').map{ it.toInt() }
     val fishState = Array<Long>(9) { 0 }
-    for(fish in fishes) {
-        fishState[fish]=fishState.getOrElse(fish) { 0 }+1
-    }
+    java.io.File("data/Day-06-data.txt").readText().split(',').map{ it.toInt() }.forEach { fish ->
+        fishState[fish]=fishState[fish]+1 }
 
     println("Day 6:")
     for(day in 1.rangeTo(256)) {
         val fishState0=fishState[0]
-        for(i in 1.rangeTo(fishState.lastIndex)) {
-            fishState[i-1]=fishState[i]
-        }
+        (1.rangeTo(fishState.lastIndex)).forEach { i-> fishState[i-1]=fishState[i] }
         fishState[6]+=fishState0
         fishState[8]=fishState0
         if( day in listOf(18,80,256))
